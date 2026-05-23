1cf8ee7f7c15496c73c28b2177213f23a95b01a4
Cassichukwu <emmanuelphilip2021@gmail.com>
2026-05-23 12:17:49 +0100

    fix cors

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import { askAgent } from "./agent.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.post("/ask", async (req, res) => {
